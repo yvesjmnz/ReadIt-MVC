25d997e75734a1479517f2dd530446ecea30eb9b
revilo121 <161302396+revilo121@users.noreply.github.com>
2024-07-11 15:06:00 +0800

    Model Post

--- models/Post.js
+++ models/Post.js
@@ -1,12 +1,38 @@
 const mongoose = require('mongoose');
 
+const commentSchema = new mongoose.Schema({
+    user: {
+        type: String,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
 const postSchema = new mongoose.Schema({
     post_description: {
         type: String,
-        required: true,
-        
+        required: true
     },
-   
+    likes: {
+        type: Number,
+        default: 0
+    },
+    dislikes: {
+        type: Number,
+        default: 0
+    },
+    comments: [commentSchema],
+    date: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 module.exports = mongoose.model('Post', postSchema);
