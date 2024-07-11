4902b5fba5aad27744d643ce5782b8307ff56a0d
revilo121 <161302396+revilo121@users.noreply.github.com>
2024-07-11 22:43:14 +0800

    Update Post.js
    updated post.js

--- models/Post.js
+++ models/Post.js
@@ -16,6 +16,12 @@ const commentSchema = new mongoose.Schema({
 });
 
 const postSchema = new mongoose.Schema({
+
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
     title: {
         type: String,
         required: true
