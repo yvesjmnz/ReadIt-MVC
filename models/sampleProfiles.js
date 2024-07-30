89bf3979b0def00c78237b2823cf0b30c9527f76
yvesjmnz <yves_alvin_jimenez@dlsu.edu.ph>
2024-07-30 17:17:49 +0800

    deleted sample users and posts

--- models/sampleProfiles.js
+++ models/sampleProfiles.js
@@ -1,35 +1,4 @@
 const sampleProfiles = [
-    {
-        username: 'DesperateOtaku',
-        profilePic: '/img/DesperateOtaku.jpg',
-        quote: 'Dokkan Dokkan tsuiteru!',
-        favoriteCommunities: ['community1', 'community2'],
-    },
-    {
-        username: 'WehSingSing1',
-        profilePic: '/img/WehSingSing1.jpg',
-        quote: '"Even the best fall down sometimes."',
-        favoriteCommunities: ['community3', 'community4'],
-    },
-    {
-        username: 'meowzzician1224',
-        profilePic: '/img/orangecar.jpg',
-        quote: 'Sometimes I am alone, sometimes I am not; you just gotta learn to live with it.',
-        favoriteCommunities: ['community3', 'community4'],
-    },
-    {
-        username: 'hungrydegen293',
-        profilePic: '/img/hungrydegen293.jpg',
-        quote: 'Long live the reckless and the brave.',
-        favoriteCommunities: ['community3', 'community4'],
-    },
-    {
-        username: 'katsuract',
-        profilePic: '/img/katsuract.jpg',
-        quote: 'im just lurking here... what are you looking at???',
-        favoriteCommunities: ['community3', 'community4'],
-    },
-    
 ];
 
 module.exports = sampleProfiles;
